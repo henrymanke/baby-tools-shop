1a4fdf669d439035aa03ea26bd755203e18a33c7
henrymanke <henry.manke@developerakademie.com>
2024-07-12 14:31:08 +0200

    ADJ | Docker improvement - optional examples v2

--- babyshop_app/start.sh
+++ babyshop_app/start.sh
@@ -3,8 +3,8 @@
 # Check if LOAD_SAMPLE_DATA is set to true
 if [ "$LOAD_SAMPLE_DATA" = "true" ]; then
   echo "Loading sample data..."
-  cp /app/sample_data/example.sqlite3 /app/db.sqlite3
-  cp -r /app/sample_data/images /app/static/images
+  cp /app/sample_data/sample.sqlite3 /app/db.sqlite3
+#   cp -r /app/sample_data/images /app/static/images
 fi
 
 # Start the application
